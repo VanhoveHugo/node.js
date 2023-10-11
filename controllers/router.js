f20b2344e3e5a9bdf1ef0b091ab5e5502ba347bd
VanhoveHugo <vanhovehugo1@gmail.com>
2023-10-11 12:27:32 +0200

    CRUD

--- controllers/router.js
+++ controllers/router.js
@@ -1,5 +1,6 @@
 const express   = require('express');
 let router      = express.Router();
+const Users    = require('../models/users');
 
 const isLogin = (req, res, next) => {
     if(req.session && req.session.user) {
@@ -13,8 +14,34 @@ router.get('/', isLogin, async (req, res) => {
     res.render('index', { user: req.session.user })
 })
 
-router.get('/login', async (req, res) => {
-    res.render('login')
+// Read All
+router.get('/users', async(req, res) => {
+    const users = await Users.find()
+    res.json(users)
+})
+// Read One by Email ( body )
+router.get('/user', async (req, res) => {
+    const email = req.body.email
+    const user = await Users.findOne({ email: email })
+    res.json(user)
+})
+// Create One ( body )
+router.post('/user', async (req, res) => {
+    const user = new Users({username: req.body.username, email: req.body.email, password: req.body.password})
+    await user.save()
+    res.json(user)
+})
+// Delete One by Email ( body )
+router.delete('/user', async (req, res) => {
+    const email = req.body.email
+    const user = await Users.deleteOne({ email: email })
+    res.json(user)
+})
+// Update One by Email ( body )
+router.put('/user', async (req, res) => {
+    const email = req.body.email
+    const user = await Users.updateOne({ email: email }, { username: req.body.username })
+    res.json(user)
 })
 
 router.post('/login', async (req, res) => {
